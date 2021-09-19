4a2084a50f69d12963dd500d66135028789ca503
AndreyWild <and1rei@mail.ru>
2021-09-19 22:42:55 +0300

    Initial commit

---
+++ src/main/java/com/wild/springsecuritydemo/model/Permission.java
@@ -0,0 +1,17 @@
+package com.wild.springsecuritydemo.model;
+
+public enum Permission {
+
+    DEVELOPER_READ("developers:read"),
+    DEVELOPER_WRITE("developers:write");
+
+    private final String permission;
+
+    Permission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+}
